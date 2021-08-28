553ea1afcdd5e0caa3949bbfc2781e9a023187e4
david <daviddecastro42@live.co.uk>
2021-08-28 16:29:01 +0100

    finished deal section

--- js/script.js
+++ js/script.js
@@ -46,6 +46,32 @@ document.querySelectorAll('.small-image-3').forEach(images => {
   };
 });
 
+let countDate = new Date('aug 1, 2021 00:00:00').getTime();
+
+function countDown() {
+  let now = new Date().getTime();
+  gap = now - countDate;
+
+  let seconds = 1000;
+  let minutes = seconds * 60;
+  let hours = minutes * 60;
+  let days = hours * 24;
+
+  let d = Math.floor(gap / days);
+  let h = Math.floor((gap % days) / hours);
+  let m = Math.floor((gap % hours) / minutes);
+  let s = Math.floor((gap % minutes) / seconds);
+
+  document.getElementById('days').innerText = d;
+  document.getElementById('hours').innerText = h;
+  document.getElementById('minutes').innerText = m;
+  document.getElementById('seconds').innerText = s;
+}
+
+setInterval(function() {
+  countDown();
+}, 1000);
+
 var swiper = new Swiper('.product-slider', {
   slidesPerView: 30,
   loop: true,
